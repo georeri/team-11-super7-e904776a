5045792e5daaf7f00294f8cf2b136bd91eb9292a
PN_JO <pavel@technoplex.net>
2023-11-16 17:06:16 -0500

    updated test_ui

--- README.md
+++ README.md
@@ -11,3 +11,6 @@ Make file contains commands needed.
 * `make test-acceptance` Runs acceptance tests
 
 * `make run` Runs the program
+{YOUR NAME} was here
+Suresh was here
+Jordan was here
---
+++ tests/test_ui.py
@@ -0,0 +1,32 @@
+# import sys
+# sys.path.append(r'/config/workspace/src')
+# from unittest import TestCase
+# from levelup.ui import GameApp
+
+
+# def test_val_fun(user_input):
+#     return user_input
+
+# test_prompt = GameApp.prompt( "Welcome, input letter:", test_val_fun)
+
+
+# print(test_obj)
+
+# test_move = GameApp.move_loop(test_prompt)
+
+
+
+
+
+
+# class TestUI:
+#     name = 'Jordan'
+
+#     self.create_character = 
+#         self.create_character()
+#         self.controller.start_game()
+#         self.starting_pos = self.controller.status.current_position
+#         self.move_loop()
+
+
+# print(GameApp.start(self))
