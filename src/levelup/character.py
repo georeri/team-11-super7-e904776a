783152d594a10a36ee6394f7335d3e0b9f32bdd9
Suresh Vishwakarma <suresh.vishwakarma@gmail.com>
2023-11-16 15:39:17 -0500

    added tests in Chararcter test

--- src/levelup/character.py
+++ src/levelup/character.py
@@ -7,7 +7,7 @@ class Character:
     # In python, we don't use getters. So no getPosition or getName for this class
     name = ""
     current_position = Position(-100,-100)
-    map :Map = Map()
+    map = Map()
 
     # Since python doesn't do method overloading, this is how we support a constructor with optional parameters
     def __init__(self, character_name=DEFAULT_CHARACTER_NAME):
@@ -17,18 +17,7 @@ class Character:
             self.name = character_name
 
     def move(self, direction :Direction) -> None:
-        # TODO: implement method here and remove the print statement below
-
-        if direction == Direction.EAST:
-            self.current_position.x = self.current_position.x + 1
-        elif direction == Direction.WEST:
-            self.current_position.x = self.current_position.x - 1
-        elif direction == Direction.NORTH:
-            self.current_position.y = self.current_position.y - 1
-        elif direction == Direction.SOUTH:
-            self.current_position.y = self.current_position.y + 1
-        else:
-            print("move method takes direction")
+        self.current_position = self.map.calculate_new_position(self.current_position, direction)
     
     def enter_map(self, map :Map) -> None:
         # TODO: implement method here and remove the print statement below
--- tests/map_double.py
+++ tests/map_double.py
@@ -6,7 +6,23 @@ class MapDouble (Map):
 
     STUBBED_X = 3
     STUBBED_Y = 4
+    starting_position = Position(0,0)
 
     def calculate_new_position(self, current_position: Position, direction: Direction) -> Position:
         # What should we return here so our character tests will work in isolation?
-        return None
+        if direction == Direction.EAST:
+            self.starting_position.x = current_position.x + 1
+            self.starting_position.y = current_position.y
+        elif direction == Direction.WEST:
+            self.starting_position.x = current_position.x - 1
+            self.starting_position.y = current_position.y
+        elif direction == Direction.NORTH:
+            self.starting_position.x = current_position.x
+            self.starting_position.y = current_position.y - 1
+        elif direction == Direction.SOUTH:
+            self.starting_position.x = current_position.x
+            self.starting_position.y = current_position.y + 1
+        else:
+            print("move method takes direction")
+
+        return self.starting_position
