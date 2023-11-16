3628c742636db3fcb8d62ca8453b6046b79632e2
Paul York <pmyork@gmail.com>
2023-11-16 16:32:42 -0500

    Updated test_map.py and map.py for calculate new position

--- src/levelup/map.py
+++ src/levelup/map.py
@@ -4,6 +4,8 @@ from levelup.direction import Direction
 
 class Map ():
 
+    MIN_ALLOWED = 0
+    MAX_ALLOWED = 9
     starting_position = Position(0,0)
     # positions = []
     size: Tuple[int, int] = (10, 10)
@@ -27,6 +29,20 @@ class Map ():
         return False        
 
     def calculate_new_position(self, current_position: Position, direction: Direction) -> Position:
-        # TODO: implement method here and remove the print statement below
-        print("calculate_new_position method not yet implemented")
-        return None
+        # What should we return here so our character tests will work in isolation?
+        if direction == Direction.EAST:
+            self.starting_position.x = min(current_position.x + 1, self.MAX_ALLOWED)
+            self.starting_position.y = current_position.y
+        elif direction == Direction.WEST:
+            self.starting_position.x = max(current_position.x - 1, self.MIN_ALLOWED)
+            self.starting_position.y = current_position.y
+        elif direction == Direction.NORTH:
+            self.starting_position.x = current_position.x
+            self.starting_position.y = max(current_position.y - 1, self.MIN_ALLOWED)
+        elif direction == Direction.SOUTH:
+            self.starting_position.x = current_position.x
+            self.starting_position.y = min(current_position.y + 1, self.MAX_ALLOWED)
+        else:
+            print("move method takes direction")
+
+        return self.starting_position
--- tests/test_map.py
+++ tests/test_map.py
@@ -47,38 +47,59 @@ class TestMap(TestCase):
         pass
 
     # # Remove comments to run this test, which will motivate you to write the production method
-    # def test_calculate_new_position_when_valid_NORTH(self):
-    #     testobj = Map()
-    #     startingPosition = Position(0,0)
-    #     expectedPosition = Position(0,1)
-    #     actualPosition = testobj.calculate_new_position(startingPosition, Direction.NORTH)
-    #     self.assertEqual(expectedPosition, actualPosition)
+    def test_calculate_new_position_when_valid_NORTH(self):
+        testobj = Map()
+        startingPosition = Position(5,5)
+        expectedPosition = Position(5,4)
+        actualPosition = testobj.calculate_new_position(startingPosition, Direction.NORTH)
+        self.assertEqual(expectedPosition, actualPosition)
 
     # Given the example above, what should these test?
     def test_calculate_new_position_when_valid_SOUTH(self):
-        # TODO: Put code here
-        pass
+        testobj = Map()
+        startingPosition = Position(2,2)
+        expectedPosition = Position(2,3)
+        actualPosition = testobj.calculate_new_position(startingPosition, Direction.SOUTH)
+        self.assertEqual(expectedPosition, actualPosition)
 
     def test_calculate_new_position_when_valid_EAST(self):
-        # TODO: Put code here
-        pass
+        testobj = Map()
+        startingPosition = Position(6,6)
+        expectedPosition = Position(7,6)
+        actualPosition = testobj.calculate_new_position(startingPosition, Direction.EAST)
+        self.assertEqual(expectedPosition, actualPosition)
 
     def test_calculate_new_position_when_valid_WEST(self):
-        # TODO: Put code here
-        pass
+        testobj = Map()
+        startingPosition = Position(2,2)
+        expectedPosition = Position(1,2)
+        actualPosition = testobj.calculate_new_position(startingPosition, Direction.WEST)
+        self.assertEqual(expectedPosition, actualPosition)
 
     def test_calculate_new_position_when_invalid_NORTH(self):
-        # TODO: Put code here
-        pass
+        testobj = Map()
+        startingPosition = Position(5,0)
+        expectedPosition = Position(5,0)
+        actualPosition = testobj.calculate_new_position(startingPosition, Direction.NORTH)
+        self.assertEqual(expectedPosition, actualPosition)
 
     def test_calculate_new_position_when_invalid_SOUTH(self):
-        # TODO: Put code here
-        pass
+        testobj = Map()
+        startingPosition = Position(5,9)
+        expectedPosition = Position(5,9)
+        actualPosition = testobj.calculate_new_position(startingPosition, Direction.SOUTH)
+        self.assertEqual(expectedPosition, actualPosition)
 
     def test_calculate_new_position_when_valid_EAST(self):
-        # TODO: Put code here
-        pass
+        testobj = Map()
+        startingPosition = Position(9,5)
+        expectedPosition = Position(9,5)
+        actualPosition = testobj.calculate_new_position(startingPosition, Direction.EAST)
+        self.assertEqual(expectedPosition, actualPosition)
 
     def test_calculate_new_position_when_valid_WEST(self):
-        # TODO: Put code here
-        pass 
+        testobj = Map()
+        startingPosition = Position(0,5)
+        expectedPosition = Position(0,5)
+        actualPosition = testobj.calculate_new_position(startingPosition, Direction.WEST)
+        self.assertEqual(expectedPosition, actualPosition)
