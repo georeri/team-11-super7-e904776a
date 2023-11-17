235e9979b672e0700fb1ba2772d283a95b78c303
Paul York <pmyork@gmail.com>
2023-11-17 11:16:43 -0500

    Initial draw_map.py code.

---
+++ src/levelup/draw_map.py
@@ -0,0 +1,25 @@
+MIN_ALLOWED = 0
+MAX_ALLOWED = 9
+map = []
+
+def make_map():
+    positions = []
+    for x in range(MIN_ALLOWED, MAX_ALLOWED + 1):
+        positions.append(["x" for y in range(MIN_ALLOWED, MAX_ALLOWED + 1)])
+    return positions
+
+def print_map(map_array):
+    for rows in map_array:
+        for item in rows:
+            print(item + " ", end="")
+        print()
+
+def update_map(map_array, x, y, player):
+    positions = map_array
+    positions[x][y] = player
+    print("\n")
+    print_map(positions)
+    
+
+map = make_map()
+print_map(map)
