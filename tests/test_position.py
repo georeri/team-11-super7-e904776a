a63611120bc07011b9c8992b7b224f981832a73a
Suresh Vishwakarma <suresh.vishwakarma@gmail.com>
2023-11-17 09:47:05 -0500

    unit test for position

--- tests/test_position.py
+++ tests/test_position.py
@@ -8,5 +8,10 @@ class TestPositionInitWithXY(TestCase):
 
         assert Pos_object.x == 3 and Pos_object.y == 4  # Assert that the x and y values are correct
         
+    def test_init1(self):
+        Pos_object1 = Position(3,4)  # Create a new position
+        Pos_object2 = Position(5,6)
+        self.assertFalse(Pos_object1 == Pos_object2) # Assert that the x and y values are correct
+        
 if __name__ == "__main__":
     unittest.main()
